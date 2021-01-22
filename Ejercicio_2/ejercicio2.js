b67285edc4b2b98dcdf621941c6f3c9334261d04
miifal <77123712+miifal@users.noreply.github.com>
2021-01-22 18:34:17 +0100

    ejercicio2

---
+++ Ejercicio_2/ejercicio2.js
@@ -0,0 +1,12 @@
+//escribir mi cumpleaños
+const celta = {
+    dia: 30,
+    mes: 'Diciembre'
+}
+
+console.log('Mi cumpleaños es el ' + celta.dia + ' de ' + celta.mes);
+
+// escribir elmes siguiente
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+console.log(meses[0]);
