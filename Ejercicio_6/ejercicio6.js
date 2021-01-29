8421cec5b4e2a6d2c1dfbd22d61bae9f26fa8673
miifal <77123712+miifal@users.noreply.github.com>
2021-01-29 19:01:30 +0100

    ejercicio6

--- Ejercicio_6/ejercicio6.js
+++ Ejercicio_6/ejercicio6.js
@@ -1,9 +1,24 @@
 
 
-
+// utilizando for ...of
 let contador = 0;
 const notas = [6, 7, 2, 9, 3, 4, 5, 8, 2];
 for (let i of notas){ 
-    console.log(contador += i)
+    contador += i;
+    console.log('la puntuacion final con for...of es ' + contador)
 };
+
+
+//utilizando for
+
+
+
+
+let contador2 = 0
+const notas2 = [6, 7, 2, 9, 3, 4, 5, 8, 2];
+for(let i2 = 0; i2 < notas2.length; i2++) {
+    contador2 += notas2[i2];
+    console.log('la puntuacion final con for es ' + contador2)
+}
+
 
