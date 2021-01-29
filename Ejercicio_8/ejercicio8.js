f05eb33cc6ec3eed448ae701c9011f65ead782e4
miifal <77123712+miifal@users.noreply.github.com>
2021-01-29 20:56:06 +0100

    ejercicio8

--- Ejercicio_8/ejercicio8.js
+++ Ejercicio_8/ejercicio8.js
@@ -4,13 +4,13 @@ const frase = prompt('Escribe una frase');
 function MayMin(texto) {
     
     if (texto === texto.toUpperCase()) {
-        resultado = "La frase es solo mayúsculas";
+        return "La frase es solo mayúsculas";
     } else if (texto === texto.toLowerCase()) {
-        resultado = "La frase es solo solo minúsculas";
+        return "La frase es solo solo minúsculas";
     } else {
-        resultado = "La frase tiene minúsculas y mayúsculas";
+        return "La frase tiene minúsculas y mayúsculas";
     }
-    return resultado
+
 }
 
 alert(MayMin(frase));
