6e4fc390149e0844e3d18a01d7246127d267eb63
miifal <77123712+miifal@users.noreply.github.com>
2021-01-29 21:28:40 +0100

    ejercicio9

---
+++ Ejercicio_9/ejercicio9.js
@@ -0,0 +1,11 @@
+
+
+const topics = ["JavaScript", "Variables", "funciones", "condicionales", "bucles"];
+
+const topicsConvers = topics.map(topic => topic.toUpperCase());
+topicsConvers.reverse();
+console.log('topics', topics);
+console.log('topicsConvers', topicsConvers);
+
+
+
