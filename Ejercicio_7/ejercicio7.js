5e1ffcf9b8100eaf25bf94a53209c6a011842ddc
miifal <77123712+miifal@users.noreply.github.com>
2021-01-29 20:11:59 +0100

    ejercicio7

---
+++ Ejercicio_7/ejercicio7.js
@@ -0,0 +1,11 @@
+// funcion qu edetecte si es numero par
+
+
+
+function pares (numero) {
+    if (numero % 2 === 0) {
+      return "Número par";
+    } else {
+      return "Número impar";
+    }
+};
