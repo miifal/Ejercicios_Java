16782d59f38d91a64cf135fd7f647dfdc4de346e
miifal <77123712+miifal@users.noreply.github.com>
2021-01-30 19:56:34 +0100

    ejercicio19

---
+++ Ejercicio_19/ejercicio19.js
@@ -0,0 +1,39 @@
+
+/*A partir del siguiente código vamos a crear varios eventos para tener una página interactiva.
+Concretamente:
+- Cuando hagamos click sobre el botón de guardar aparecerá un alert. 
+- Cuando hagamos foco sobre el input del nombre el fondo será de un color y cuando se lo quitemos, de otro.
+- Dependiendo si escribimos una vocal o una consonante en el segundo input su contenido será de un color distinto.*/
+
+
+document.querySelector(".save").addEventListener("click", saveData);
+
+function saveData() {
+  alert('Se han guardado los datos');
+};
+
+// Al quitar el foco del input cambia de color
+const inputName = document.querySelector(".name");
+
+inputName.addEventListener('focus', changeColorFocus);
+function changeColorFocus() {
+  inputName.style.backgroundColor = '#fff666';
+};
+
+inputName.addEventListener('focusout', changeColorNoFocus);
+function changeColorNoFocus() {
+  inputName.style.backgroundColor = '#9999FF';
+};
+
+// Si la letra que pulsemos es vocal lo pintaremos de rojo y de verde si es consonante
+const inputRandom = document.querySelector('.random');
+
+inputRandom.addEventListener('keypress', checkLetter);
+function checkLetter(event){
+  const currentLetter = event.which;
+  if (currentLetter === 97 || currentLetter === 101 || currentLetter === 105 || currentLetter === 111 || currentLetter === 117) {
+    inputRandom.style.color = '#a8323e';
+  } else {
+    inputRandom.style.color = '#32a836';
+  }
+};
